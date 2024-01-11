cd421cf8d279b20452a7046c08b8ef622b83ba6e
nathaniel nosa <nathanielnarth@gmail.com>
2024-01-11 09:41:13 +0100

    Redux setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import RouterSetup from './routers/RouterSetup.jsx'
 
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <RouterSetup />
+  // </React.StrictMode>,
+  <Provider store={store}>
     <RouterSetup />
-  </React.StrictMode>,
+  </Provider>,
 )
