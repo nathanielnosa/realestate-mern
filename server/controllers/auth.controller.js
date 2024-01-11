854e6063cd32cbfdeb71ffa1a3bd6b0c2c979a98
nathaniel nosa <nathanielnarth@gmail.com>
2024-01-11 03:35:52 +0100

    sign-in api routes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -12,4 +13,24 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
-}   
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validEmail = await userModel.findOne({ email })
+        if (!validEmail) return next(errorHandler(404, 'Wrong Credentials!'))
+        const validPassword = bcryptjs.compareSync(password, validEmail.password)
+        if (!validPassword) return next(errorHandler(401, 'Wrong Credentials!'))
+
+        const token = jwt.sign({ id: validEmail._id }, process.env.JWT_SECRET)
+        // hide password not to be licked to users
+        const { password: pwd, ...otherInfo } = validEmail._doc
+
+        res.cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(otherInfo)
+    } catch (error) {
+        next(error)
+    }
+}
